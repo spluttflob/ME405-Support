35465f8b23b608f51299ce568f67c001dd1e828f
Professor Spluttflob <none@not.net>
2023-02-19 22:21:09 -0800

    Bug fixes, test code, and cleanup in mma845x_shell.py

--- examples/mma845x_shell.py
+++ examples/mma845x_shell.py
@@ -4,64 +4,67 @@ This file contains a @b partly @b written MicroPython driver for the MMA8451
 and MMA8452 accelerometers. It is intended to be used as a starting point for
 an exercise in a mechatronics course. 
 
+This is a STM32 (Pyboard, Nucleo) only version. A more portable version would
+use @c machine.I2C instead of @c pyb.I2C.
+
 @author JR Ridgely
 @copyright GPL Version 3.0
 """
-
-import micropython
+import pyb
+from micropython import const
 
 
 ## The register address of the STATUS register in the MMA845x
-STATUS_REG = micropython.const (0x00)
+STATUS_REG = const (0x00)
 
 ## The register address of the OUT_X_MSB register in the MMA845x
-OUT_X_MSB = micropython.const (0x01)
+OUT_X_MSB = const (0x01)
 
 ## The register address of the OUT_X_LSB register in the MMA845x
-OUT_X_LSB = micropython.const (0x02)
+OUT_X_LSB = const (0x02)
 
 ## The register address of the OUT_Y_MSB register in the MMA845x
-OUT_Y_MSB = micropython.const (0x03)
+OUT_Y_MSB = const (0x03)
 
 ## The register address of the OUT_Y_LSB register in the MMA845x
-OUT_Y_LSB = micropython.const (0x04)
+OUT_Y_LSB = const (0x04)
 
 ## The register address of the OUT_Z_MSB register in the MMA845x
-OUT_Z_MSB = micropython.const (0x05)
+OUT_Z_MSB = const (0x05)
 
 ## The register address of the OUT_Z_LSB register in the MMA845x
-OUT_Z_LSB = micropython.const (0x06)
+OUT_Z_LSB = const (0x06)
 
 ## The register address of the WHO_AM_I register in the MMA845x
-WHO_AM_I = micropython.const (0x0D)
+WHO_AM_I = const (0x0D)
 
 ## The register address of the DATA_CFG_REG register in the MMA845x which is
 #  used to set the measurement range to +/-2g, +/-4g, or +/-8g
-XYZ_DATA_CFG = micropython.const (0x0E)
+XYZ_DATA_CFG = const (0x0E)
 
 ## The register address of the CTRL_REG1 register in the MMA845x
-CTRL_REG1 = micropython.const (0x2A)
+CTRL_REG1 = const (0x2A)
 
 ## The register address of the CTRL_REG2 register in the MMA845x
-CTRL_REG2 = micropython.const (0x2B)
+CTRL_REG2 = const (0x2B)
 
 ## The register address of the CTRL_REG3 register in the MMA845x
-CTRL_REG3 = micropython.const (0x2C)
+CTRL_REG3 = const (0x2C)
 
 ## The register address of the CTRL_REG4 register in the MMA845x
-CTRL_REG4 = micropython.const (0x2D)
+CTRL_REG4 = const (0x2D)
 
 ## The register address of the CTRL_REG5 register in the MMA845x
-CTRL_REG5 = micropython.const (0x2E)
+CTRL_REG5 = const (0x2E)
 
 ## Constant which sets acceleration measurement range to +/-2g
-RANGE_2g = micropython.const (0)
+RANGE_2g = const (0)
 
 ## Constant which sets acceleration measurement range to +/-2g
-RANGE_4g = micropython.const (1)
+RANGE_4g = const (1)
 
 ## Constant which sets acceleration measurement range to +/-2g
-RANGE_8g = micropython.const (2)
+RANGE_8g = const (2)
 
 
 class MMA845x:
@@ -77,12 +80,14 @@ class MMA845x:
     
     An example of how to use this driver:
     @code
-    mma = mma845x.MMA845x (pyb.I2C (1, pyb.I2C.MASTER, baudrate = 100000), 29)
+    i2c = pyb.I2C (1, pyb.I2C.CONTROLLER, baudrate = 100000)
+    mma = mma845x.MMA845x (i2c, 29)
     mma.active ()
     mma.get_accels ()
     @endcode 
     The example code works for an MMA8452 on a SparkFun<sup>TM</sup> breakout
-    board. """
+    board.
+    """
 
     def __init__ (self, i2c, address, accel_range = 0):
         """! Initialize an MMA845x driver on the given I<sup>2</sup>C bus. The 
@@ -94,12 +99,11 @@ class MMA845x:
         @param accel_range The range of accelerations to measure; it must be
             either @c RANGE_2g, @c RANGE_4g, or @c RANGE_8g (default: 2g)
         """
-
         ## The I2C driver which was created by the code which called this
-        self.i2c = i2c
+        self._i2c = i2c
 
         ## The I2C address at which the accelerometer is located
-        self.addr = address
+        self._addr = address
 
         # Request the WHO_AM_I device ID byte from the accelerometer
         self._dev_id = ord (i2c.mem_read (1, address, WHO_AM_I))
@@ -124,18 +128,17 @@ class MMA845x:
         mode, the accelerometer's settings can't be messed with. Active mode
         is set by setting the @c ACTIVE bit in register @c CTRL_REG1 to one.
         """
-
         if self._works:
-            reg1 = ord (self.i2c.mem_read (1, self.addr, CTRL_REG1))
+            reg1 = ord (self._i2c.mem_read (1, self._addr, CTRL_REG1))
             reg1 |= 0x01
-            self.i2c.mem_write (chr (reg1), self.addr, CTRL_REG1)
+            self._i2c.mem_write (chr (reg1), self._addr, CTRL_REG1)
 
 
     def standby (self):
         """! Put the MMA845x into standby mode so its settings can be changed.
         No data will be taken in standby mode, so before measurements are to
-        be made, one must call @c active(). """
-
+        be made, one must call @c active().
+        """
         if self._works:
             reg1 = ord (self._i2c.mem_read (1, self._addr, CTRL_REG1))
             reg1 &= ~0x01
@@ -145,8 +148,8 @@ class MMA845x:
     def get_ax_bits (self):
         """! Get the X acceleration from the accelerometer in A/D bits and 
         return it.
-        @return The measured X acceleration in A/D conversion bits """
-
+        @return The measured X acceleration in A/D conversion bits
+        """
         print ('MMA845x clueless about X acceleration')
         return 0
 
@@ -154,8 +157,8 @@ class MMA845x:
     def get_ay_bits (self):
         """! Get the Y acceleration from the accelerometer in A/D bits and 
         return it.
-        @return The measured Y acceleration in A/D conversion bits """
-
+        @return The measured Y acceleration in A/D conversion bits
+        """
         print ('MMA845x clueless about Y acceleration')
         return 0
 
@@ -163,8 +166,8 @@ class MMA845x:
     def get_az_bits (self):
         """! Get the Z acceleration from the accelerometer in A/D bits and 
         return it.
-        @return The measured Z acceleration in A/D conversion bits """
-
+        @return The measured Z acceleration in A/D conversion bits
+        """
         print ('MMA845x clueless about Z acceleration')
         return 0
 
@@ -172,8 +175,8 @@ class MMA845x:
     def get_ax (self):
         """! Get the X acceleration from the accelerometer in g's, assuming
         that the accelerometer was correctly calibrated at the factory.
-        @return The measured X acceleration in g's """
-
+        @return The measured X acceleration in g's
+        """
         print ('MMA845x uncalibrated X')
         return 0
 
@@ -182,8 +185,8 @@ class MMA845x:
         """! Get the Y acceleration from the accelerometer in g's, assuming
         that the accelerometer was correctly calibrated at the factory. The
         measurement is adjusted for the range (2g, 4g, or 8g) setting.
-        @return The measured Y acceleration in g's """
-
+        @return The measured Y acceleration in g's
+        """
         print ('MMA845x uncalibrated Y')
         return 0
 
@@ -192,8 +195,8 @@ class MMA845x:
         """! Get the Z acceleration from the accelerometer in g's, assuming
         that the accelerometer was correctly calibrated at the factory. The
         measurement is adjusted for the range (2g, 4g, or 8g) setting.
-        @return The measured Z acceleration in g's """
-
+        @return The measured Z acceleration in g's
+        """
         print ('MMA845x uncalibrated Z')
         return 0
 
@@ -201,8 +204,8 @@ class MMA845x:
     def get_accels (self):
         """! Get all three accelerations from the MMA845x accelerometer. The
         measurement is adjusted for the range (2g, 4g, or 8g) setting.
-        @return A tuple containing the X, Y, and Z accelerations in g's """
-
+        @return A tuple containing the X, Y, and Z accelerations in g's
+        """
         return (self.get_ax (), self.get_ay (), self.get_az ())
 
 
@@ -210,18 +213,24 @@ class MMA845x:
         """! 'Convert' The MMA845x accelerometer to a string. The string 
         contains information about the configuration and status of the
         accelerometer. 
-        @return A string containing diagnostic information """
-
+        @return A string containing diagnostic information
+        """
         if not self._works:
             return ('No working MMA845x at I2C address ' + str (self.addr))
         else:
-            reg1 = ord (self.i2c.mem_read (1, self.addr, CTRL_REG1))
+            reg1 = ord (self._i2c.mem_read (1, self._addr, CTRL_REG1))
             diag_str = 'MMA845' + str (self._dev_id >> 4) \
-                + ': I2C address ' + hex (self.addr) \
+                + ': I2C address ' + hex (self._addr) \
                 + ', Range=' + str (1 << (self._range + 1)) + 'g, Mode='
             diag_str += 'active' if reg1 & 0x01 else 'standby'
 
             return diag_str
 
 
+# Very simple test code which should be improved
+if __name__ == "__main__":
+    i2c = pyb.I2C(1, pyb.I2C.CONTROLLER)
+    mma = MMA845x(i2c, 29)
+    mma.active()
+    print(f"Accel: {mma.get_ax():.3f},{mma.get_ay():.3f},{mma.get_az():.3f} g")
 
